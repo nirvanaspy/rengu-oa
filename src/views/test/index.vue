4d2a5cfa156f09daa04819e47688d7066fe4385a
nirvana <nirvana_spy@outlook.com>
2018-12-13 23:35:39 +0800

    提交测试

---
+++ src/views/test/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            test
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
